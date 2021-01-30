50be6baf591fa0b95296cbee24d274f369287477
aliemtakarai <aliemtakarai@gmail.com>
2021-01-30 20:36:33 +0800

    add firebase function database

--- flask-firebase/__init__.py
+++ flask-firebase/__init__.py
@@ -3,16 +3,23 @@ import os
 import flask
 import pyrebase
 from flask import render_template
+from flask import request, redirect
 
 app = flask.Flask(__name__)
+app.config.from_pyfile('./setting.py')
 
 # firebase config
 config = {
-    "databaseURL": app.config.get('firebase_database_url'),
-    "serviceAccount": app.config.get('firebase_credential')
+    "apiKey": app.config.get('FIREBASE_API_KEY'),
+    "authDomain": app.config.get('FIREBASE_AUTH_DOMAIN'),
+    "databaseURL": app.config.get('FIREBASE_DATABASE_URL'),
+    "storageBucket": app.config.get('FIREBASE_STORAGE_BUCKET'),
+    "serviceAccount": app.config.get('FIREBASE_CREDENTIAL')
 }
 
 firebase = pyrebase.initialize_app(config)
+
+db = firebase.database()
 
 def create_app():
     app = Flask(__name__)
@@ -20,8 +27,20 @@ def create_app():
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = db.child('post').get()
+    return render_template('index.html', data = data)
 
-@app.route('/create-post')
+@app.route('/create-post', methods=['GET','POST'])
 def create():
-    return render_template('create-post.html')
+    if request.method == 'GET':
+        return render_template('create-post.html')
+    elif request.method == 'POST':
+        data = dict(request.form)
+        db.child("post").push(data)
+        return redirect('/')
+
+@app.route('/delete-post')
+def destroy():
+    id = request.args.get('data')
+    db.child('post').child(id).remove()
+    return redirect('/')
